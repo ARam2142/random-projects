83d390beb88b76902896fe7b1752956a5d5dcb6c
Aram2142 <andresaaramirez@aol.com>
2021-02-28 00:09:30 -0500

    hexavalue challenge in progress

---
+++ projects/Change HexColor/index.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('color');
+const body = document.getElementById('transform');
+const colorName = document.getElementById('value');
+
+let colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FF00',  '#800080'];
+for (let i = 0; i < colors.length; i++) {
+    colors.push(i);
+      
+}
