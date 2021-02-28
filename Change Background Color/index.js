8c87924e6b65cc58d9ab5db88b72c68959c6923c
Aram2142 <andresaaramirez@aol.com>
2021-02-27 22:27:52 -0500

    change background color challenge complete

---
+++ Change Background Color/index.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('color');
+const body = document.getElementById('change');
+let colorArr = ['red', 'blue', 'green', 'orange', 'yellow', 'violet', ''];
+
+function changeColor() {
+    let randomColor = Math.round(Math.random() * colorArr.length);
+    console.log(randomColor);
+
+    body.style.backgroundColor = colorArr[randomColor]
+}
+
+button.addEventListener('click', function() {
+    changeColor()
+})
